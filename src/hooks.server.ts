30cde13307476db1812fec8518d1b1ace8a5ba2a
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-09-06 07:59:53 +0530

    Log error stack on internal server error

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,11 @@
 import { error, type HandleServerError } from '@sveltejs/kit';
 
 export const handleError: HandleServerError = ({ error: err }) => {
-	if (err instanceof Error && err.message.includes('duplicate')) {
-		throw error(409, err.message);
+	if (err instanceof Error) {
+		if (err.message.includes('duplicate')) {
+			throw error(409, err.message);
+		}
+
+		console.log(err.stack);
 	}
 };
