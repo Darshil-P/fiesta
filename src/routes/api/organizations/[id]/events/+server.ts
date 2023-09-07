8883d19a8274f9b65e041a2db73a16f0c8820acf
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-09-07 14:26:33 +0530

    Add organization events endpoint

---
+++ src/routes/api/organizations/[id]/events/+server.ts
@@ -0,0 +1,31 @@
+import { db } from '$lib/server/db';
+import { jsonResponse } from '$lib/server/helper';
+import { eventsTable } from '$lib/server/schema';
+import { error, type RequestHandler } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import Joi from 'joi';
+
+export const GET = (async ({ params }) => {
+	const organizationId = Number.parseInt(params.id ?? '');
+
+	const requestSchema = Joi.number().required();
+
+	const { error: validationError } = requestSchema.validate(organizationId);
+
+	if (validationError) {
+		throw error(400, validationError.details[0].message);
+	}
+
+	const events = await db
+		.select()
+		.from(eventsTable)
+    .where(eq(eventsTable.organizationId, organizationId));
+
+	if (events.length == 0) {
+		throw error(404, 'Events Not Found');
+	}
+
+	const organizationEvents = events;
+
+	return jsonResponse(JSON.stringify(organizationEvents));
+}) satisfies RequestHandler;
