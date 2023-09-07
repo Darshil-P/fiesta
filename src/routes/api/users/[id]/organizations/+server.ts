b5277575afbb470bd9846864722b9b76219bd48f
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-09-07 15:26:56 +0530

    Add user organizations endpoint

---
+++ src/routes/api/users/[id]/organizations/+server.ts
@@ -0,0 +1,37 @@
+import { db } from '$lib/server/db';
+import { jsonResponse } from '$lib/server/helper';
+import { organizationMembersTable, organizationsTable } from '$lib/server/schema';
+import { error, type RequestHandler } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import Joi from 'joi';
+
+export const GET = (async ({ params }) => {
+	const userId = Number.parseInt(params.id ?? '');
+
+	const requestSchema = Joi.number().required();
+
+	const { error: validationError } = requestSchema.validate(userId);
+
+	if (validationError) {
+		throw error(400, validationError.details[0].message);
+	}
+
+	const organizations = await db
+		.select({ organizationsTable })
+		.from(organizationsTable)
+		.innerJoin(
+			organizationMembersTable,
+			eq(organizationMembersTable.organizationId, organizationsTable.organizationId)
+		)
+		.where(eq(organizationMembersTable.userId, userId));
+
+	if (organizations.length == 0) {
+		throw error(404, 'Events Not Found');
+	}
+
+	const userEvents = organizations.map((organization) => {
+		return organization.organizationsTable;
+	});
+
+	return jsonResponse(JSON.stringify(userEvents));
+}) satisfies RequestHandler;
