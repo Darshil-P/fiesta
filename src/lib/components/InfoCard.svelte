ca633f66e4d87c635e6a6aa2321b7b98496291dc
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-09-15 09:56:45 +0530

    Add info card

---
+++ src/lib/components/InfoCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+	export let imageUrl: string;
+	export let title: string;
+	export let verified: boolean;
+</script>
+
+<div class="card min-w-[320px] max-w-[272px] p-4">
+	<div class="flex flex-row">
+		<Avatar src={imageUrl} width="max-w-[64px]" rounded="rounded-full" />
+		<h4 class="h4 my-auto ml-4 mr-auto h-8 overflow-hidden whitespace-nowrap overflow-ellipsis">
+			{title}
+		</h4>
+		{#if verified}
+			<span class="material-symbols-outlined my-auto pb-1">verified</span>
+		{/if}
+	</div>
+</div>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,5 @@
 export { default as AppBar } from '$lib/components/AppBar.svelte';
 export { default as EventCard } from '$lib/components/EventCard.svelte';
 export { default as Footer } from '$lib/components/Footer.svelte';
+export { default as InfoCard } from '$lib/components/InfoCard.svelte';
 export { default as ScrollContainer } from '$lib/components/ScrollContainer.svelte';
