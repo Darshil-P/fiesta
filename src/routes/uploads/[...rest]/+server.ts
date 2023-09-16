1d3fff312bfe2e54c8e0270e92d745504f4ca3d2
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-09-16 23:46:26 +0530

    Add volume & endpoint for user uploaded images

---
+++ src/routes/uploads/[...rest]/+server.ts
@@ -0,0 +1,16 @@
+import { error, type RequestHandler } from '@sveltejs/kit';
+import { promises as fs } from 'fs';
+
+export const GET = (async ({ params }) => {
+	const path = params.rest;
+	try {
+		const asset = await fs.readFile(`src/lib/uploads/${path}.webp`);
+		return new Response(asset, {
+			headers: {
+				'Content-Type': 'image/webp'
+			}
+		});
+	} catch (_) {
+		throw error(404, 'Image Not Found');
+	}
+}) satisfies RequestHandler;
