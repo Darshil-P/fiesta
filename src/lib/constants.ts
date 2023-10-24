7ba4bfcdac9b17080a9a40cca3805db3ee68eca4
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-10-24 12:49:31 +0530

    Add payment details to booking summary page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,7 @@ export const PHONE_REGEX = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$
 export const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export const PASSWORD_REGEX =
 	/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+export const UPIID_REGEX = /^[a-zA-Z0-9.\-_]{2,256}@[a-zA-Z]{2,64}$/;
+export const CARDNUMBER_REGEX =
+	/^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
+export const CARDCVV_REGEX = /^\d{3,4}$/;
--- src/routes/buy/[eventId]/+page.svelte
+++ src/routes/buy/[eventId]/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { CARDCVV_REGEX, CARDNUMBER_REGEX, PHONE_REGEX, UPIID_REGEX } from '$lib/constants';
+	import type { FormError } from '$lib/types';
 	import type { PageServerData } from './$types';
+
+	let walletBalance = 5000;
+	let formValid = true;
 
 	const paymentMethods = [
 		{ key: 'wallet', value: 'Wallet' },
@@ -9,11 +14,115 @@
 		{ key: 'debit_card', value: 'Debit Card' }
 	];
 
+	const formData = {
+		upiId: ''
+	};
+
+	const cardData = {
+		cardNumber: '',
+		nameOnCard: '',
+		expiryMonth: '',
+		expiryYear: '',
+		cvv: ''
+	};
+
+	const formError: FormError = {
+		upiId: null
+	};
+
+	const cardError: FormError = {
+		cardNumber: '',
+		nameOnCard: '',
+		cvv: ''
+	};
+
+	let formVerify: FormError = {
+		upiId: null
+	};
+
+	const validateForm = () => {
+		if (paymentMethod == paymentMethods[0]) {
+			return (formValid = true);
+		}
+
+		if (paymentMethod == paymentMethods[1]) {
+			return (formValid = formVerify.upiId != null);
+		}
+
+		if (paymentMethod == paymentMethods[2] || paymentMethod == paymentMethods[3]) {
+			return (formValid = !(
+				Object.values(cardError).some((err) => err != null) ||
+				Object.values(cardData).some((v) => v === '') != false
+			));
+		}
+
+		formValid = false;
+	};
+
+	const validateUPI = () => {
+		formValid = false;
+		formVerify.upiId = null;
+
+		if (formData.upiId == '') {
+			return (formError.upiId = 'Field Cannot be Empty');
+		}
+
+		if (UPIID_REGEX.test(formData.upiId)) {
+			return (formError.upiId = null);
+		}
+
+		if (PHONE_REGEX.test(formData.upiId)) {
+			return (formError.upiId = null);
+		}
+
+		formError.upiId = 'Invalid UPI Id';
+	};
+
+	const verifyUPI = (event: MouseEvent) => {
+		event.preventDefault();
+		validateUPI();
+
+		if (!formError.upiId) {
+			formValid = true;
+			return (formVerify.upiId = 'UPI Verified!');
+		}
+		formVerify.upiId = null;
+	};
+
+	const validateCardNumber = () => {
+		formValid = false;
+		if (cardData.cardNumber == '') {
+			return (cardError.cardNumber = 'Card Number is required');
+		}
+
+		if (!CARDNUMBER_REGEX.test(cardData.cardNumber)) {
+			return (cardError.cardNumber = 'Invalid Card Number');
+		}
+
+		cardError.cardNumber = null;
+		validateForm();
+	};
+
+	const validateCVV = () => {
+		formValid = false;
+
+		if (cardData.cvv == '') {
+			return (cardError.cvv = 'CVV is required');
+		}
+
+		if (!CARDCVV_REGEX.test(cardData.cvv)) {
+			return (cardError.cvv = 'Invalid CVV');
+		}
+
+		cardError.cvv = null;
+		validateForm();
+	};
+
 	export let data: PageServerData;
 
 	const { ticketDetails } = data;
 
-	let paymentMethod = 'wallet';
+	let paymentMethod = paymentMethods[0];
 </script>
 
 <div class="card variant-ghost-surface mx-auto my-16 max-w-md p-8">
@@ -51,22 +160,132 @@
 				{#each paymentMethods as method}
 					<button
 						type="button"
-						class="chip text-sm font-bold {paymentMethod === method.key
+						class="chip text-sm font-bold {paymentMethod === method
 							? 'variant-filled'
 							: 'variant-outline-surface'}"
 						on:click={() => {
-							paymentMethod = method.key;
+							paymentMethod = method;
+							validateForm();
 						}}
-						on:keypress
 					>
 						<span>{method.value}</span>
 					</button>
 				{/each}
 			</label>
 			<hr />
+
+			{#if paymentMethod == paymentMethods[0]}
+				Your Wallet Balance is: {walletBalance}
+				<br />
+				Balance after payment: {walletBalance - ticketDetails.price}
+			{/if}
+
+			{#if paymentMethod == paymentMethods[1]}
+				<label class="label">
+					<span class="font-bold">UPI ID</span>
+					<div class="input-group input-group-divider grid-cols-[1fr_auto]">
+						<input
+							bind:value={formData.upiId}
+							on:input={validateUPI}
+							type="text"
+							class="input p-2 rounded-token"
+						/>
+						<button
+							class="variant-filled-surface font-semibold"
+							on:click={(event) => verifyUPI(event)}>Verify</button
+						>
+					</div>
+
+					{#if formError.upiId}
+						<div class="variant-ghost-error p-0.5 px-2 text-xs">{formError.upiId}</div>
+					{/if}
+
+					{#if formVerify.upiId}
+						<div class="variant-ghost-success p-0.5 px-2 text-xs">{formVerify.upiId}</div>
+					{/if}
+				</label>
+			{/if}
+
+			{#if paymentMethod == paymentMethods[2] || paymentMethod == paymentMethods[3]}
+				<div class="grid grid-cols-2 gap-4">
+					<label class="label col-span-2">
+						<span class="font-bold">{paymentMethod.value} Number</span>
+						<input
+							bind:value={cardData.cardNumber}
+							on:input={validateCardNumber}
+							class="input p-2"
+							type="text"
+							maxlength="100"
+							required
+						/>
+						{#if cardError.cardNumber}
+							<div class="variant-ghost-error p-0.5 px-2 text-xs">{cardError.cardNumber}</div>
+						{/if}
+					</label>
+					<label class="label col-span-2">
+						<span class="font-bold">Name on Card</span>
+						<input
+							bind:value={cardData.nameOnCard}
+							on:input={() =>
+								(cardError.nameOnCard = cardData.nameOnCard == '' ? 'Name cannot be empty' : null)}
+							class="input p-2"
+							type="text"
+							maxlength="100"
+							placeholder="Name on Card"
+							required
+						/>
+						{#if cardError.nameOnCard}
+							<div class="variant-ghost-error p-0.5 px-2 text-xs">{cardError.nameOnCard}</div>
+						{/if}
+					</label>
+					<label class="label">
+						<span class="font-bold">Expiry Date</span>
+						<div class="input-group input-group-divider grid-cols-[4fr_5fr]">
+							<input
+								bind:value={cardData.expiryMonth}
+								type="number"
+								min={new Date().toISOString().substring(5, 7)}
+								placeholder="MM"
+								style="color-scheme: dark;"
+								class="input w-full p-2 rounded-token"
+							/>
+							<input
+								bind:value={cardData.expiryYear}
+								type="number"
+								min={new Date().toISOString().substring(0, 4)}
+								placeholder="YYYY"
+								style="color-scheme: dark;"
+								class="input w-full p-2 rounded-token"
+							/>
+						</div>
+					</label>
+
+					<label class="label">
+						<span class="font-bold">CVV</span>
+						<br />
+						<input
+							bind:value={cardData.cvv}
+							on:input={validateCVV}
+							type="text"
+							minlength="3"
+							maxlength="3"
+							class="input w-full p-2 rounded-token"
+						/>
+						{#if cardError.cvv}
+							<div class="variant-ghost-error p-0.5 px-2 text-xs">{cardError.cvv}</div>
+						{/if}
+					</label>
+				</div>
+			{/if}
+
+			<hr />
 		{/if}
 		<footer>
-			<button type="submit" class="variant-filled-primary btn w-full font-bold">
+			<button
+				type="submit"
+				disabled={!formValid}
+				class="variant-filled-primary btn w-full font-bold"
+			>
 				Confirm
 				{#if ticketDetails.type == 1}
 					Payment
